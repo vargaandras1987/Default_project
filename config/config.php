50a4e67c5b242d09d74ce680047351ff66afe80f
vargaandras1987 <71977042+vargaandras1987@users.noreply.github.com>
2021-02-27 15:54:30 +0100

    Update config.php
    config törlése

--- config/config.php
+++ config/config.php
@@ -1,24 +1,4 @@
-<?php
-//Config fájlok kerülnek ide
-define('ONE_DB_HOST', 'localhost');
-define('ONE_DB_USER', 'root');
-define('ONE_DB_PASS', '');
-define('ONE_DB_NAME', 'projectone');
 
-define('TWO_DB_HOST', 'eu-cdbr-west-03.cleardb.net');
-define('TWO_DB_USER', 'bf1a1eca12e9a2');
-define('TWO_DB_PASS', '631c0423');
-define('TWO_DB_NAME', 'heroku_c96365f2311b91c');
-
-define('LOGPATH','C:\\xampp2\\htdocs\\Default_project_git\\logs\\'.date('Y-m-d'));
-define('TWIGPATH','C:\\xampp2\\htdocs\\Default_project_git\\public\\html');
-
-define('MAIL_SEND_HOST','smtp.gmail.com');
-define('MAIL_SEND_PORT','587');
-define('MAIL_USERNAME','devilslate@gmail.com');
-define('MAIL_PASSWORD','Cseger1987');
-define('MAIL_FROMNAME','Andras');
-define('MAIL_FROM','devilslate@gmail.com');
 
 
 
